896f4a5999b560ba547e88e926ec6202c6e00e44
thomaswkm <t.kinderman01@ufromail.cl>
2022-09-08 18:05:14 -0400

    test

---
+++ src/main/java/UfroStore.java
@@ -0,0 +1,8 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class UfroStore {
+    public static void main(String[] args) {
+        System.out.println("hola");
+        System.out.println("test");
+    }
+}
